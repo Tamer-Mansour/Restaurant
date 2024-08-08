4b2cde5feb8356f6c86eefa145c19842330967e1
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2024-08-08 13:28:43 +0300

    Models and Seeding

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -2,6 +2,9 @@
 // (WebApplication.CreateBuilder) then pass the (args)
 // (args): It is a set of parameters that are passed to the web
 // application from the command line (such as configuration options).
+using Restaurants.Infrastructure.Extensions;
+using Restaurants.Infrastructure.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,8 +13,15 @@ var builder = WebApplication.CreateBuilder(args);
 // that measn our application used the controller for hanle requests
 builder.Services.AddControllers();
 
+
+builder.Services.AddInfrastructre(builder.Configuration);
+
 // build the application after after set all services
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+await seeder.Seed();
 
 // Configure the HTTP request pipeline.
 // is middleware that can gide requests to use the spicific protocal
---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtenstions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Seeders;
+
+namespace Restaurants.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtenstions
+    {
+        public static void AddInfrastructre(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("RestaurantsDb");
+            services.AddDbContext<RestaurantDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+        }
+    }
+}
